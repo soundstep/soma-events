b3933ef527c39a7f989467f010b06f3133af7baf
Romuald Quantin <romu@soundstep.com>
2012-09-14 03:37:21 +0100

    in progress

---
+++ tests/lib/utils.js
@@ -0,0 +1,66 @@
+;(function(utils, undefined) {
+
+	utils.applyProperties = function(target, extension) {
+		for (var prop in extension) {
+			target[prop] = extension[prop];
+		}
+	};
+
+	utils.inherit = function(target, obj) {
+		var subclass;
+		if (obj && obj.hasOwnProperty('constructor')) {
+			// use constructor if defined
+			subclass = obj.constructor;
+		} else {
+			// call the super constructor
+			subclass = function(){
+				return target.apply(this, arguments);
+			};
+		}
+		// add super properties
+		utils.applyProperties(subclass.prototype, target.prototype);
+		// set the prototype chain to inherit from the parent without calling parent's constructor
+		var chain = function(){};
+		chain.prototype = target.prototype;
+		subclass.prototype = new chain();
+		// add obj properties
+		if (obj) utils.applyProperties(subclass.prototype, obj, target.prototype);
+		// point constructor to the subclass
+		subclass.prototype.constructor = subclass;
+		// set super class reference
+		subclass.parent = target.prototype;
+		return subclass;
+	};
+
+	if (!Function.prototype.bind) {
+		Function.prototype.bind = function bind(that) {
+			var target = this;
+			if (typeof target != "function") {
+				throw new Error("Error, you must bind a function.");
+			}
+			var args = Array.prototype.slice.call(arguments, 1); // for normal call
+			var bound = function () {
+				if (this instanceof bound) {
+					var F = function(){};
+					F.prototype = target.prototype;
+					var self = new F;
+					var result = target.apply(
+						self,
+						args.concat(Array.prototype.slice.call(arguments))
+					);
+					if (Object(result) === result) {
+						return result;
+					}
+					return self;
+				} else {
+					return target.apply(
+						that,
+						args.concat(Array.prototype.slice.call(arguments))
+					);
+				}
+			};
+			return bound;
+		};
+	};
+
+})(this['utils'] = this['utils'] || {});
